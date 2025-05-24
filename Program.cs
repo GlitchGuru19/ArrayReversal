f7f25674c38194bbb003c46bc9b9dd40a65af99d
GlitchGuru19 <peterkatebe12@gmail.com>
2025-05-24 20:04:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Learning about array reversal
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[]
+        {
+            0,1,2,3,4,5
+        };
+
+        Array.Reverse(numbers);
+
+        foreach (int number in numbers)
+        {
+            Console.Write($"{number} ");
+        }
+
+        Console.ReadLine();
+    }
+}
